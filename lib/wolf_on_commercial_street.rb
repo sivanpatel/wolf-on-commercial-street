a28a0ea3b008a81b6440d1318c6f704f2a5abed6
Ptolemy <ptolemybarnes@gmail.com>
2015-09-04 13:01:45 +0100

    Adds travis and sets up test

---
+++ lib/wolf_on_commercial_street.rb
@@ -0,0 +1,9 @@
+class WolfOnCommercialStreet
+
+  def initialize(stock_prices)
+  end
+
+  def maximise_profits
+  end
+
+end
--- spec/wolf_on_commercial_street_spec.rb
+++ spec/wolf_on_commercial_street_spec.rb
@@ -1,1 +1,12 @@
 require 'wolf_on_commercial_street'
+
+describe WolfOnCommercialStreet do
+
+  it 'calculates the optimal buy and sell days' do
+    wolfy = WolfOnCommercialStreet.new([10,2,5,8,9,1,6,7,9,8])
+    
+    expect(wolfy.maximise_profits).to eq(
+      "Buy on day 6 and sell on day 9"
+    )
+  end
+end
